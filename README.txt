ff3152a8682b95ab162b85d5f75aa115d8298910
AJDstudios <aaronjdodd1989@gmail.com>
2022-11-26 15:01:54 +0000

    about1

--- README.txt
+++ README.txt
@@ -1,10 +1,28 @@
 The Author site For Aaron James Dodd
+    -- Current Workload week beginning 26/11/22 -- 
+        -- Goals for this sprint --    
+            - dark/light toggle works - angel/demon icon if possible
+            - CSS on contact
+            - About page
+        
+        -- About page --
+        HTML framework [x]
+        CSS[x]
+        Content[x]
+        bigger screens CSS []
+
+        -- Contact --
+        CSS []
+        General contact info 
+        include twitch and youtube
+
+    -- commit map (timeline not included until commit #9 as this was a sideproject until then) --
     -- intial commit --
         created app
         made a readme with a list of work to complete
         Confirmed it worked
 
-    -- Navigation --
+    -- Navigation init--
 
     -- Commit 1 --
         Icons and logos changed
@@ -34,6 +52,7 @@ The Author site For Aaron James Dodd
     -- Commit 6 (forgot to update this) --
         burger and desktop menu now separate
 
+    --HOME init--
     -- Commit 7 --
         Home page content added
 
@@ -42,15 +61,19 @@ The Author site For Aaron James Dodd
         edited header and footer colours
         added contact form ( needs CSS )
 
-        Current to do
-            - dark/light toggle works - angel/demon icon if possible
-            - CSS on contact
-            - About page
+    -- ABOUT INIT --
+    -- CONTACT Init --
+    -- Commit 9 -- 26/11/22 --
+        About and Contact HTML framework added.
+        First pass at content on About page
+        contact form 
+        recapcha for contact
+        css for mobile for About page (this will likely be heavily edited) 
 
     -- remember --
         !Mobile first
 
-    -- Work flow (not timelined for now as this is project 2)
+    -- Overall Work flow (Broad Notes)
         - Navbar - fully functional and sticky. [x]
         - footer with dark/light toggle []
         - Home [x]
@@ -62,4 +85,4 @@ The Author site For Aaron James Dodd
         - login []
         - forum []
         - language switcher [] 
-        - launch website []
+        - launch website [] 10/01/2023 initial target (update weekly)
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,36 @@
 import React from "react";
+import FravashiAD from "../images/fravashiad.webp"
 
 class About extends React.Component{
     render(){
         return (
-            <h1>About Aaron James Dodd</h1>
+            <div className="about">
+                <div className="abouthead">
+                    <h1>
+                        Aaron James Dodd
+                    </h1>
+                    <img alt="3DAD" src={ FravashiAD } />
+                </div>
+                <div className="aboutauthor">
+                    <h2>The Author</h2>
+                    <p>Aaron has been writing since a young child, and "Demons" was one of his first "finished" works. It started as a film script where that scene now resides in book 5 in the series.
+                       Much of his experience was gained through writing communities, but during university he ran a website that allowed unknown authors to get there work out there, running a short 
+                       story series and a weekly upload schedule for journalism and fiction both.  
+                    </p>
+                </div>
+                <div className="aboutcreative">
+                    <h2>The Creative</h2>
+                    <p>Writing is only one pursuit, Aaron also makes games in the Demons Universe, writes music and creates sprites and 3d art that come to life in these projects. 
+                       Anything related to software, video editing, streaming, design and game dev is self-taught and realised. His background in music is slightly more formal though, even working as a private guitar tutor 
+                       from the age of 15.  
+                    </p>
+                </div>
+                <div className="About the man">
+                    <h2>The Man</h2>
+                    <p>Though Aaron has worked in many fields, the majority of his time has been spent abroad teaching English as a foreign language. France, the Czech Republic and Russia have been 
+                        long-term homes as well as many places in the UK. Now he's returned and shifted his focus to software engineering. He works as a frontend software engineer in Cardiff today.</p>
+                </div>    
+            </div>
         )
     }
 }
