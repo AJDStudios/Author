84a32c858ce6642b7ecf93386c9d77ff1992246c
AJDstudios <aaronjdodd1989@gmail.com>
2023-01-08 16:22:50 +0000

    added image and rearranged author page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import FravashiAD from "../images/fravashiad.webp"
+import AngelAD from "../images/angelad.webp"
 
 class About extends React.Component{
     render(){
@@ -9,10 +9,12 @@ class About extends React.Component{
                     <h1>
                         Aaron James Dodd
                     </h1>
-                    <img alt="3DAD" src={ FravashiAD } />
+                    
                 </div>
                 <div className="aboutauthor">
                     <h2>The Author</h2>
+                    {/* Use a professional shoot*/} 
+                    <img alt="3DAD" src={ AngelAD } />
                     <p>Aaron has been writing since a young child, and "Demons" was one of his first "finished" works. It started as a film script where that scene now resides in book 5 in the series.
                        Much of his experience was gained through writing communities, but during university he ran a website that allowed unknown authors to get there work out there, running a short 
                        story series and a weekly upload schedule for journalism and fiction both.  
@@ -20,6 +22,7 @@ class About extends React.Component{
                 </div>
                 <div className="aboutcreative">
                     <h2>The Creative</h2>
+                    <img alt="3DAD" src={ AngelAD } />
                     <p>Writing is only one pursuit, Aaron also makes games in the Demons Universe, writes music and creates sprites and 3d art that come to life in these projects. 
                        Anything related to software, video editing, streaming, design and game dev is self-taught and realised. His background in music is slightly more formal though, even working as a private guitar tutor 
                        from the age of 15.  
@@ -27,6 +30,8 @@ class About extends React.Component{
                 </div>
                 <div className="About the man">
                     <h2>The Man</h2>
+                    {/* Use a regular photo*/} 
+                    <img alt="3DAD" src={ AngelAD } />
                     <p>Though Aaron has worked in many fields, the majority of his time has been spent abroad teaching English as a foreign language. France, the Czech Republic and Russia have been 
                         long-term homes as well as many places in the UK. Now he's returned and shifted his focus to software engineering. He works as a frontend software engineer in Cardiff today.</p>
                 </div>    
